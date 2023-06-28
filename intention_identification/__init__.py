18a8a24d4b63d5b71cc2b398eb684927f5dacce1
jackjiang <28810274@qq.com>
2023-06-28 15:26:07 +0800

    删除所有项目！

--- intention_identification/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
-
-def get_project_absolute_path(path):
-    return os.path.abspath(os.path.join(ROOT_DIR, path))
--- intention_identification/commons.py
+++
@@ -1,5 +0,0 @@
-"""定义通用模块"""
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
